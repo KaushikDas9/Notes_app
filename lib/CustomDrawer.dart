23e2041d20f15fa064379c23645135b7c28c7080
KaushikDas9 <dasbabai2017@gmail.com>
2023-10-04 22:39:50 +0530

    u

--- lib/CustomDrawer.dart
+++ lib/CustomDrawer.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class myCustomDrawer extends StatefulWidget {
@@ -11,9 +10,70 @@ class myCustomDrawer extends StatefulWidget {
 class _myCustomDrawerState extends State<myCustomDrawer> {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Container( color: Colors.black,),
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
 
+    return Drawer(
+      child: Container(
+          color: Colors.black.withOpacity(.85),
+          child: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  height: height * .05,
+                  
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(left: 30, top: 10),
+                  child: const Text(
+                    "Google Keep",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  // color: Colors.white,
+                  width:width ,alignment: Alignment.centerLeft,margin: EdgeInsets.only(left: 8,top: 25),
+                  // height: height * .875,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
+                          Text("Notes",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),)
+                        ],
+                      ),
+                       Row(
+                        children: [
+                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
+                          Text("Notes",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),)
+                        ],
+                      ),
+                       Row(
+                        children: [
+                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
+                          Text("Notes",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),)
+                        ],
+                      ),
+                       Row(
+                        children: [
+                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
+                          Text("Notes",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),)
+                        ],
+                      ),
+                       Row(
+                        children: [
+                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
+                          Text("Notes",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),)
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
