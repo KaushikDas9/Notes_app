d43bbe2cb24e7a8e618deaf901805c85d0eaec04
KaushikDas9 <dasbabai2017@gmail.com>
2023-10-05 17:01:42 +0530

    uploaded

--- lib/CustomDrawer.dart
+++ lib/CustomDrawer.dart
@@ -8,6 +8,8 @@ class myCustomDrawer extends StatefulWidget {
 }
 
 class _myCustomDrawerState extends State<myCustomDrawer> {
+
+  bool sectionOneEffect=false;bool sectionTwoEffect=false;bool sectionThreeEffect=false;bool sectionFourEffect=false;bool sectionFiveEffect=false;bool sectionSixEffect=false;bool sectionSevenEffect=false;  
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -21,7 +23,6 @@ class _myCustomDrawerState extends State<myCustomDrawer> {
               children: [
                 Container(
                   height: height * .05,
-                  
                   alignment: Alignment.centerLeft,
                   margin: EdgeInsets.only(left: 30, top: 10),
                   child: const Text(
@@ -35,53 +36,110 @@ class _myCustomDrawerState extends State<myCustomDrawer> {
                 Divider(),
                 Container(
                   // color: Colors.white,
-                  width:width ,alignment: Alignment.centerLeft,margin: EdgeInsets.only(left: 8,top: 5),
+                  width: width, alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(left: 0, top: 5),
                   // height: height * .875,
                   child: Column(
                     children: [
+
+
+                    
+                       
+                         Container(
+                          // color: Color.fromARGB(255, 159, 25, 25),
+                          // width: width,
+                          margin: EdgeInsets.only(right: 20),
+                          child: TextButton(
+
+                            style:sectionOneEffect ? sectionButtonStyle() : null ,
+                            onPressed: (){ setState(() {
+                              sectionOneEffect=true;
+                              sectionTwoEffect=false;
+                              sectionThreeEffect=false;
+                              sectionFourEffect=false;
+                              sectionFiveEffect=false;
+                              sectionSixEffect=false;
+                              sectionSevenEffect=false;
+                            }); }  ,
+                            child: const Row(
+                              children: [
+                                IconButton(
+                                    onPressed: null,
+                                    icon:  Icon(
+                                      Icons.lightbulb,
+                                      color: Colors.white,
+                                    )),
+                                Text(
+                                  "Notes",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      
+                      sectiontwo(),  
+
+                      
+                    
+                      Divider(),
+                      sectionthree(), 
+                      
+                      Divider(),
+                     sectionfour(),
                       Row(
                         children: [
-                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
-                          Text("Notes",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),)
-                        ],
-                      ),
-                       Row(
-                        children: [
-                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
-                          Text("Reminders",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),)
-                        ],
-                      ),
-                      Divider(),
-                       Row(
-                        children: [
-                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
-                          Text("Create new label",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),)
-                        ],
-                      ),
-                      Divider(),
-                       Row(
-                        children: [
-                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
-                          Text("Archive",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),)
-                        ],
-                      ),
-                       Row(
-                        children: [
-                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
-                          Text("Trash",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),)
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.lightbulb,
+                                color: Colors.white,
+                              )),
+                          Text(
+                            "Trash",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          )
                         ],
                       ),
                       Divider(),
-                       Row(
+                      Row(
                         children: [
-                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
-                          Text("Settings",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),)
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.lightbulb,
+                                color: Colors.white,
+                              )),
+                          Text(
+                            "Settings",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          )
                         ],
                       ),
-                       Row(
+                      Row(
                         children: [
-                          IconButton(onPressed: () {} , icon: Icon(Icons.lightbulb, color: Colors.white,)),
-                          Text("Help & feedback",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),)
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.lightbulb,
+                                color: Colors.white,
+                              )),
+                          Text(
+                            "Help & feedback",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          )
                         ],
                       ),
                     ],
@@ -92,4 +150,122 @@ class _myCustomDrawerState extends State<myCustomDrawer> {
           )),
     );
   }
+
+  ButtonStyle sectionButtonStyle() { 
+    return ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orange.withOpacity(.8)) ,shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomRight: Radius.circular(50), topRight: Radius.circular(50)))) ) ;
+  }
+ Widget sectiontwo() { 
+  return  Container(
+                          // color: Color.fromARGB(255, 159, 25, 25),
+                          // width: width,
+                          margin: EdgeInsets.only(right: 20),
+                          child: TextButton(
+
+                            style:sectionTwoEffect ? sectionButtonStyle() : null ,
+                            onPressed: (){ setState(() {
+                              sectionOneEffect=false;
+                              sectionTwoEffect=true;
+                              sectionThreeEffect=false;
+                              sectionFourEffect=false;
+                              sectionFiveEffect=false;
+                              sectionSixEffect=false;
+                              sectionSevenEffect=false;
+                            }); }  ,
+                            child: Row(
+                              children: [
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.notifications,
+                                      color: Colors.white,
+                                    )),
+                                const Text(
+                                  "Reminders",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+ } 
+
+Widget sectionthree() { 
+  return  Container(
+                          // color: Color.fromARGB(255, 159, 25, 25),
+                          // width: width,
+                          margin: EdgeInsets.only(right: 20),
+                          child: TextButton(
+
+                            style:sectionThreeEffect ? sectionButtonStyle() : null ,
+                            onPressed: (){ setState(() {
+                              sectionOneEffect=false;
+                              sectionTwoEffect=false;
+                              sectionThreeEffect=true;
+                              sectionFourEffect=false;
+                              sectionFiveEffect=false;
+                              sectionSixEffect=false;
+                              sectionSevenEffect=false;
+                            }); }  ,
+                            child: Row(
+                              children: [
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                    )),
+                                const Text(
+                                  "Create new Label",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+ } 
+
+Widget sectionfour() { 
+  return  Container(
+                          // color: Color.fromARGB(255, 159, 25, 25),
+                          // width: width,
+                          margin: EdgeInsets.only(right: 20),
+                          child: TextButton(
+
+                            style:sectionFourEffect ? sectionButtonStyle() : null ,
+                            onPressed: (){ setState(() {
+                              sectionOneEffect=false;
+                              sectionTwoEffect=false;
+                              sectionThreeEffect=false;
+                              sectionFourEffect=true;
+                              sectionFiveEffect=false;
+                              sectionSixEffect=false;
+                              sectionSevenEffect=false;
+                            }); }  ,
+                            child: Row(
+                              children: [
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.archive,
+                                      color: Colors.white,
+                                    )),
+                                const Text(
+                                  "Archive",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+ } 
+
 }
